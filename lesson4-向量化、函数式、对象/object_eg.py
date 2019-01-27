a37b6f4e6bba059c6929a31fe66cbf5b9e03eb34
deerain <deerainw@gmail.com>
2019-01-27 23:42:34 +0800

    update

--- lesson4-向量化、函数式、对象/object_eg.py
+++ lesson4-向量化、函数式、对象/object_eg.py
@@ -3,7 +3,7 @@
 """
 
 
-#%%
+#%%  定义对象
 class Human:
     def __init__(self):
         self.gender = 'male'
@@ -13,6 +13,7 @@ class Human:
         print("大家好，我是 " + self.name)
 
 
-#%%
+#%% 生成实例
 Trump = Human()
 Trump.sum()
+Trump.gender
